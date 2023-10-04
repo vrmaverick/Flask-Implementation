74fd288ce5d75ec403b9dd92a6b841130cd6d7a3
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-10-04 23:52:44 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 print(__name__)
-@app.route('/')
+@app.route('/')#http://127.0.0.1:5000/
 def hello_world():
-    return render_template('wordle.html') #clock.htm is rendered
-@app.route('/wordle.html')
+    return render_template('wordle.html') #clock.html is rendered
+@app.route('/wordle.html') # try http://127.0.0.1:5000/wordle.html
 def wordle():
     return render_template('clock.html') #wordle game  on diffrent page
+@app.route('/<username>/<blog>/<int:blog_id>')#http://127.0.0.1:5000/Vedant/Using_Flask/2023
+def show(username=None,blog =None,blog_id = None):
+    return render_template("blog.html",name = username,blog_name = blog,blog_id = blog_id)#defining variable rules they change dynamically in html
 
